84cc0ea820308149f3396a5f665edb72db9aa095
Van Eyck Jo <jo.vaneyck@gmail.com>
2021-11-12 19:42:18 +0100

    first end to end scenario: we find playtimes for the first finished game in my collection!

--- time-played-videogames/Program.fs
+++ time-played-videogames/Program.fs
@@ -9,5 +9,14 @@ let main argv =
         games
         |> List.filter (fun g -> g.Shelf = Grouvee.Shelf.Played)
 
+    let first = finishedGames |> Seq.head
+
+    let searchResult =
+        first.Title
+        |> HowLongToBeat.getHtml
+        |> HowLongToBeat.parseSearchResult
+
+    printfn "Search result: %A" searchResult
+
     printfn "Wow, you completed %d games! You badass!" (finishedGames |> Seq.length)
     0
