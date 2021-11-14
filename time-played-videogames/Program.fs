1f0fcd3e9d67ae2bb4a313a337f0abbdccfa0e1f
Van Eyck Jo <jo.vaneyck@gmail.com>
2021-11-14 11:19:30 +0100

    async in main

--- time-played-videogames/Program.fs
+++ time-played-videogames/Program.fs
@@ -7,20 +7,23 @@ let main argv =
     let grouveeCsvPath = argv.[0]
     let games = Grouvee.parseFile grouveeCsvPath
 
-    let finishedGames =
-        games
-        |> List.filter (fun g -> g.Shelf = Grouvee.Shelf.Played)
-
     let responses =
         finishedGames
         |> List.map
             (fun game ->
-                printfn "Querying HLTB for %s" game.Title
-                let result = game.Title |> HowLongToBeat.getHtml
-                printfn "Finished querying HLTB for %s" game.Title
-                result)
+                async {
+                    printfn "Querying HLTB for %s" game.Title
+                    let! result = game.Title |> HowLongToBeat.getHtml
+                    printfn "Finished querying HLTB for %s" game.Title
+                    return result
+                })
         |> Async.ParallelWithThrottle 10
         |> Async.RunSynchronously
+
+    let parsed =
+        responses
+        |> Seq.map HowLongToBeat.parseSearchResult
+        |> Seq.toList
 
     printfn "Wow, you completed %d games! You badass!" (finishedGames |> Seq.length)
     0
