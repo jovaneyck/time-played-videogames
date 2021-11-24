046aa7b3154d05751b285642a29aca556f817e2f
Van Eyck Jo <jo.vaneyck@gmail.com>
2021-11-24 20:59:20 +0100

    quick end to end test

--- time-played-videogames/tests/EndToEndTests.fs
+++ time-played-videogames/tests/EndToEndTests.fs
@@ -3,5 +3,13 @@
 open Xunit
 open Swensen.Unquote
 
+open App
+
 [<Fact>]
-let ``Can run the app end to end for a very small collection`` () = ()
+let ``Can run the app end to end for a very small collection`` () =
+    let games, results = run id @".\tests\e2e-grouvee.csv"
+
+    test <@ games |> List.length = 1 @>
+    test <@ games.[0].Title = "Super Mario Odyssey" @>
+    test <@ results.Completionist > 50m @>
+    test <@ results.Completionist < 100m @>
